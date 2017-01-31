cade1569224386a6f6a7044dae7bccdbdf5c68da
Maxime Guillemette <maxime_glmt@hotmail.ca>
2017-01-30 20:40:21 -0500

    termine fichier de test compass

--- code_arduino/test_compass/test_compass.ino
+++ code_arduino/test_compass/test_compass.ino
@@ -1,16 +1,35 @@
+
+
+#include "Arduino.h"
+#include "Wire.h"
+#include "Adafruit_Sensor.h"
+#include "Adafruit_HMC5883_U.h"
 #include "Compas.h"
 
-#define MAG_DECLINAISON 0.1745
+#define MAG_DECLINAISON 0
 
-
-Compas compass(MAG_DECLINAISON);
-
+// Compas compass(MAG_DECLINAISON)
+Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+// compass.init();
+//mag = Adafruit_HMC5883_Unified(12345);
+float northAngle = 0;
 
 void setup(){
-    compass.init();
+    Serial.begin(9600);
+    mag.begin();
 }
 
 void loop(){
-    getNorthAngle();
+    sensors_event_t event;
+    mag.getEvent(&event);
+    northAngle = atan2(event.magnetic.y, event.magnetic.x);
+    northAngle += MAG_DECLINAISON;
+    if(northAngle < 0){
+        northAngle += 2*PI;
+    }
+    else if(northAngle > 2*PI){
+        northAngle -= 2*PI;
+    }
+    Serial.println(northAngle);
     delay(500);
 }
