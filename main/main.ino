3806f1ff9cdf595a1af9c1256a1431134b7397b4
Maxime Guillemette <maxime_glmt@hotmail.ca>
2017-05-15 19:35:51 -0400

    ajout des includes pour le main

---
+++ main/main.ino
@@ -0,0 +1,23 @@
+/*
+    Programme principal pour l'acquisition de données de la station au sol
+    du GAUL - 2017.
+    Ver.    0.1
+    Simon Bellemare
+    Samuel Côté
+    Maxime Guillemette
+*/
+
+#include "lib/acquisitionLogger.h"
+#include "lib/Compas.h"
+#include "lib/rocketData.h"
+#include "lib/serialPort.h"
+#include "lib/TinyGPS.h"
+#include "lib/transceiver.h"
+
+void setup(){
+
+}
+
+void loop(){
+
+}
