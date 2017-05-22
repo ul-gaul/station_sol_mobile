2ea9b46c64eb0657215be0b3d077f8a8f9d83d93
Maxime Guillemette <maxime_glmt@hotmail.ca>
2017-05-22 14:49:42 -0400

    ajout de define pour les pins du arduino dans le main

--- main/main.ino
+++ main/main.ino
@@ -15,16 +15,37 @@
 #include "lib/transceiver.h"
 #include "Arduino.h"
 
-#define RFD_PORT 1                                  
+#define RFD_PORT 1
 #define GPS_PORT 2
-#define SD_PIN 53
+// pin SDA 20
+// pin SCL 21 pour le Compas
+#define LCD_D0 22
+#define LCD_D1 23
+#define LCD_D2 24
+#define LCD_D3 25
+#define LCD_D4 26
+#define LCD_D5 27
+#define LCD_D6 28
+#define LCD_D7 29
+#define LCD_CS1 34
+#define LCD_CS2 33
+#define LCD_RW 35
+#define LCD_RS 36
+#define LCD_E 37
+#define BTN_1 49
+#define BTN_2 47
+#define BTN_3 45
+#define BTN_4 43
+// carte SD: MISO 50
+// MOSI 51, SCK 52, CS 53
+#define SD_CHIP_SELECT 53
 
 #define RFD_BAUDRATE 57600
 #define GPS_BAUDRATE 9600
 
 #define MAG_DECLINAISON 0
-#define LOG_TIME_INTERVAL 1000  
-#define SYSTEM_TYPE "r"    
+#define LOG_TIME_INTERVAL 1000
+#define SYSTEM_TYPE "r"
 
 float northAngle = 0;
 float latitude = 0;
@@ -37,6 +58,8 @@ TinyGPS gps;
 SerialPort serial_RX(RFD_PORT_NB, RFD_BAUD_RATE);
 Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
 AcquisitionLogger sdLogger(SD_PIN, SYSTEM_TYPE);
+
+typedef enum menus {MAIN, WATCH_ROCKET, FIND_ROCKET} menus;
 
 void Log_Start(){
   while(!test_init_sd){
@@ -93,4 +116,5 @@ void loop(){
   Log_Rocket_Data();  //Receives Data from the rocket and logs it into the SD card
   Get_North_Angle();  //Gets the orientation of the base station
   Get_Position();     //Gets the position of the base station
+
 }
