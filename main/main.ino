57dc155aaa7d44444f0e7b6e1a70010d53926ce7
Maxime Guillemette <maxime_glmt@hotmail.ca>
2017-06-11 14:32:45 -0400

    amélioré code du main, cleanup et ajout de commentaires

--- main/main.ino
+++ main/main.ino
@@ -1,7 +1,7 @@
 /*
     Main program to acquire and log data from rocket.
-    Groupe Aerospatial de l'Universite Laval
-    Ver.    0.5
+    Groupe Aerospatial de l'Universite Laval, Avionics Team
+    Ver.    1.0
     Simon Bellemare
     Samuel Côté
     Maxime Guillemette
@@ -23,8 +23,6 @@
 #define RFD_BAUDRATE 57600
 #define GPS_PORT_NB 2
 #define GPS_BAUDRATE 9600
-// pin SDA 20
-// pin SCL 21 pour le Compas
 #define LCD_D0 22
 #define LCD_D1 23
 #define LCD_D2 24
@@ -43,27 +41,26 @@
 #define BTN_2 3
 #define BTN_3 45
 #define BTN_4 43
-// carte SD: MISO 50
-// MOSI 51, SCK 52, CS 53
 #define SD_CHIP_SELECT 53
 
 #define MAG_DECLINAISON 0
 #define LOG_TIME_INTERVAL 1000
 #define SYSTEM_TYPE "r"
 #define PI 3.141593
+#define SUBMENU_TABLE_SIZE 2
 
-
-
+// UART declarations for RFD900
 Transceiver RFD_RX;
 RocketPacket rpktRX;
+SerialPort serial_RX(RFD_PORT_NB, RFD_BAUDRATE);
+
+// Declarations for GPS
 TinyGPS gps;
 SerialPort serialGPS(GPS_PORT_NB, GPS_BAUDRATE);
 bool newData = false;
 
-// UART declarations for RFD900
-SerialPort serial_RX(RFD_PORT_NB, RFD_BAUDRATE);
 // Declarations for Compass
-Adafruit_HMC5883_Unified mag = Adafruit_HMC5883_Unified(12345);
+Adafruit_HMC5883_Unified compass = Adafruit_HMC5883_Unified(12345);
 sensors_event_t compass_mag_event;
 float north_angle = 0;
 float rocket_angle = 0;
@@ -71,44 +68,42 @@ float bts_latitude = 0;
 float bts_longitude = 0;
 float rocket_latitude = 0;
 float rocket_longitude = 0;
-float distance_to_rocket;
+float distance_to_rocket = 0;
+
 //Declarations for SD card
 AcquisitionLogger sdLogger(SD_CHIP_SELECT, SYSTEM_TYPE);
 bool test_init_sd = false;
 
 // Declarations for menus
 typedef enum menu {MAIN, WATCH_ROCKET, FIND_ROCKET} menu;
-#define SUBMENU_TABLE_SIZE 2
 // Array of menus that can be selected from the main menu
 menu submenu_table[SUBMENU_TABLE_SIZE] = {WATCH_ROCKET, FIND_ROCKET};
-
 menu current_menu = MAIN;
-
 bool btn1_ispressed = false;
 bool btn2_ispressed = false;
 bool btn3_ispressed = false;
 bool btn4_ispressed = false;
-
 sas_lcd_menu lcd_display;
 
+/* Initializes the SD card and writes the CVS header in the file */
 void Log_Start(){
-    while(!test_init_sd){
+    int sd_card_init_fails = 0;
+    while((!test_init_sd) and (sd_card_init_fails < 1000)){
         test_init_sd = sdLogger.initCard();
+        if(!test_init_sd){
+            Serial.println("SD card init failed");
+            sd_card_init_fails++;
+        } else {
+            Serial.println("SD card init sucess");
+        }
     }
-
     sdLogger.writeHeader();
 }
 
-void Log_Rocket_Data(){
-    if (serial_RX.available()){
-        rpktRX = RFD_RX.receiveRocketPacket(rpktRX, serial_RX);
-        sdLogger.writeRocketData(rpktRX);
-    }
-}
-
+/* Uses the compass to return the angle (in radians) to the north */
 float get_north_angle(){
     sensors_event_t event;
-    mag.getEvent(&event);
+    compass.getEvent(&event);
     north_angle = atan2(event.magnetic.y, event.magnetic.x);
     north_angle += MAG_DECLINAISON;
     if(north_angle < 0){
@@ -120,8 +115,11 @@ float get_north_angle(){
     return north_angle;
 }
 
-/* Donne l'angle à partir du nord pour rejoindre la cible */
-float get_rocket_angle(float lat1, float lon1, float lat2, float lon2, float north_angle) {
+/* Uses GPS coordinates and angle to north to return the rocket's angle from
+    the north */
+float get_rocket_angle(float lat1, float lon1, float lat2, float lon2,
+                        float north_angle) {
+
     float RocketAngle = atan2(abs(lon1 - lon2), abs(lat1 - lat2));
     if(lon2 > lon1){
         RocketAngle = (PI / 2) - RocketAngle;
@@ -138,26 +136,7 @@ float get_rocket_angle(float lat1, float lon1, float lat2, float lon2, float nor
     return RocketAngle;
 }
 
-void Get_Position (){
-    for (unsigned long start = millis(); millis() - start < 1000;)
-    {
-        while (serialGPS.available())
-        {
-            char c = serialGPS.read();
-            if (gps.encode(c))
-            {
-                newData = true;
-            }
-        }
-        if (newData)
-        {
-            unsigned long age;
-            gps.f_get_position(&bts_latitude, &bts_longitude, &age);
-        }
-    }
-}
-
-/* function used to get the distance between the rocket and the base station */
+/* Returns distance between the rocket and the base station */
 float get_distance_to_rocket(float lat1, float long1, float lat2, float long2) {
     distance_to_rocket = gps.distance_between(lat1, long1, lat2, long2);
     if (distance_to_rocket > 100000) {
@@ -166,25 +145,25 @@ float get_distance_to_rocket(float lat1, float long1, float lat2, float long2) {
     return distance_to_rocket;
 }
 
-/* interrupt service routine for when button 1 is pressed */
+/* Interrupt service routine for when button 1 is pressed */
 void int_button1_pressed(){
     Serial.println("btn1 pressed!");
     btn1_ispressed = true;
 }
 
-/* interrupt service routine for when button 2 is pressed */
+/* Interrupt service routine for when button 2 is pressed */
 void int_button2_pressed(){
     Serial.println("btn2 pressed!");
     btn2_ispressed = true;
 }
 
-/* interrupt service routine for when button 3 is pressed */
+/* Interrupt service routine for when button 3 is pressed */
 void int_button3_pressed(){
     Serial.println("btn3 pressed!");
     btn3_ispressed = true;
 }
 
-/* interrupt service routine for when button 4 is pressed */
+/* Interrupt service routine for when button 4 is pressed */
 void int_button4_pressed(){
     Serial.println("btn4 pressed!");
     btn4_ispressed = true;
@@ -206,7 +185,6 @@ void main_menu(menu* p_menu, menu submenu_table[SUBMENU_TABLE_SIZE]){
     Serial.println("MAIN MENU\n");
     Serial.println("Watch rocket\nFind Rocket\nPress ok to select a menu");
     Serial.println("Btn1: OK, Btn2: DOWN");
-    // interrupts();
     lcd_display.main_menu("Watch rocket");
     while (1){
         if(selected_menu == WATCH_ROCKET){
@@ -222,12 +200,10 @@ void main_menu(menu* p_menu, menu submenu_table[SUBMENU_TABLE_SIZE]){
             Serial.println("button OK pressed");
             *p_menu = selected_menu;
             set_all_buttons_false();
-            // noInterrupts();
             break;
         }
         if(btn2_ispressed){
             // button DOWN
-            // change selected menu DOWN
             Serial.println("button DOWN pressed");
             Serial.print("index is: ");
             Serial.println(index);
@@ -242,7 +218,6 @@ void main_menu(menu* p_menu, menu submenu_table[SUBMENU_TABLE_SIZE]){
         }
         if(btn3_ispressed){
             // button UP
-            // change selected menu UP
             Serial.println("button UP pressed");
             Serial.print("index is: ");
             Serial.println(index);
@@ -257,7 +232,7 @@ void main_menu(menu* p_menu, menu submenu_table[SUBMENU_TABLE_SIZE]){
     }
 }
 
-/* function for the menu that allows to watch te Rocket's flight */
+/* function for the menu that allows to watch the Rocket's flight */
 void watch_rocket_menu(menu* p_menu){
     Serial.println("Watch Rocket Menu");
     Serial.println("Press back to go to main menu");
@@ -305,23 +280,22 @@ void find_rocket_menu(menu* p_menu){
     Serial.println("Press back to go to main menu");
     Serial.println("Press watch to go to watch rocket menu");
     Serial.println("BACK: btn1, WATCH: btn2");
+    unsigned long age;
     while (1){
         // Receive RocketPackets
         rpktRX = RFD_RX.receiveRocketPacket(rpktRX, serial_RX);
         // Log data in SD card
         sdLogger.writeRocketData(rpktRX);
-        // Display rocket data
-        // Interesting to find rocket is: distance from rocket, direction to
-        // rocket
-        // Get_Position();
-        unsigned long age;
+        // Get GPS data
         for (unsigned long start = millis(); millis() - start < 1000;)
         {
             while (serialGPS.available())
             {
                 char c = serialGPS.read();
-                // Serial.write(c); // uncomment this line if you want to see the GPS data flowing
-                if (gps.encode(c)) { // Did a new valid sentence come in?
+                // Uncomment this line if you want to see the GPS data flowing
+                // Serial.write(c);
+                // Did a new valid sentence come in?
+                if (gps.encode(c)) {
                     newData = true;
                 }
             }
@@ -331,18 +305,25 @@ void find_rocket_menu(menu* p_menu){
             rocket_latitude = rpktRX.rocketData.latitude1;
             rocket_longitude = rpktRX.rocketData.longitude1;
             gps.f_get_position(&bts_latitude, &bts_longitude, &age);
+            // Display GPS data
             Serial.print("BTS lat: ");
             Serial.println(bts_latitude);
             Serial.print("BTS lon: ");
             Serial.println(bts_longitude);
         }
-        distance_to_rocket = get_distance_to_rocket(bts_latitude, bts_longitude, rocket_latitude, rocket_longitude);
+        // Display rocket data
+        // Interesting to find rocket is: distance from rocket, direction to
+        // rocket
+        distance_to_rocket = get_distance_to_rocket(bts_latitude, bts_longitude,
+                                                    rocket_latitude, rocket_longitude);
         Serial.print("Distance to rocket is: ");
         Serial.println(distance_to_rocket);
         north_angle = get_north_angle();
         Serial.print("Angle to north is: ");
         Serial.println(north_angle*180/PI);
-        rocket_angle = get_rocket_angle(bts_latitude, bts_longitude, rocket_latitude, rocket_longitude, north_angle);
+        rocket_angle = get_rocket_angle(bts_latitude, bts_longitude,
+                                        rocket_latitude, rocket_longitude,
+                                        north_angle);
         Serial.print("Angle to rocket from north is: ");
         Serial.println(rocket_angle*180/PI);
         lcd_display.find_rocket_menu(distance_to_rocket,
@@ -366,27 +347,17 @@ void find_rocket_menu(menu* p_menu){
 }
 
 void setup(){
-    // noInterrupts();
     Serial.begin(9600);
     Serial.println("Setup begin");
+    // Initialize RFD900 serial port
     serial_RX.begin();
+    // Initialize GPS serial port
     serialGPS.begin();
-    mag.begin();
-    // Serial2.begin();
+    // Initialize compass
+    compass.begin();
+    // Initialize SD card
     Log_Start();
-    // Init SD card
-    bool test_sd_card = false;
-    while(!test_init_sd){
-        test_init_sd = sdLogger.initCard();
-        if(!test_init_sd){
-            Serial.println("SD card init failed");
-        } else {
-            Serial.println("SD card init sucess");
-        }
-    }
-    sdLogger.writeHeader();
-    // attach interrupt for buttons
-    // interrupts();
+    // Attach interrupt for buttons
     pinMode(BTN_1, INPUT);
     pinMode(BTN_2, INPUT);
     pinMode(BTN_3, INPUT);
@@ -395,11 +366,17 @@ void setup(){
     attachInterrupt(digitalPinToInterrupt(BTN_2), int_button2_pressed, FALLING);
     attachInterrupt(digitalPinToInterrupt(BTN_3), int_button3_pressed, FALLING);
     attachInterrupt(digitalPinToInterrupt(BTN_4), int_button4_pressed, FALLING);
+    // Initialize LCD screen
     GLCD.Init();
     GLCD.SelectFont(LCD_FONT);
     Serial.println("Setup end");
 }
 
+/*  Main program for the base station. Works by switching over different menus
+    using interrupts attached to buttons and interracting with user on a LCD
+    display. The global variable current_menu is driven by those interrupts.
+    Each menu has it's own function that is called depending of which action
+    the user takes. */
 void loop(){
 
     Serial.println("switching menus");
