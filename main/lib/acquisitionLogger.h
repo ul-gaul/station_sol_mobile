6fe360e982a742aa10a17b8bcdc82f1855d3e5ac
Maxime Guillemette <maxime_glmt@hotmail.ca>
2017-06-18 00:32:40 -0400

    adaptation de la librairie acquisitionLogger pour la dernière version de la structure rocketData

--- main/lib/acquisitionLogger.h
+++ main/lib/acquisitionLogger.h
@@ -24,9 +24,6 @@ sur une carte SD.
 #define ACCEL_X "Acceleration X"
 #define ACCEL_Y "Acceleration Y"
 #define ACCEL_Z "Acceleration Z"
-#define MAGNET_X "Magnetic Field X"
-#define MAGNET_Y "Magnetic Field Y"
-#define MAGNET_Z "Magnetic Field Z"
 #define ALTITUDE "Altitude"
 #define LATITUDE1 "Latitude 1"
 #define LONGITUDE1 "Longitude 1"
@@ -34,22 +31,43 @@ sur une carte SD.
 #define LONGITUDE2 "Longitude 2"
 #define TEMP_1 "Temperature 1"
 #define TEMP_2 "Temperature 2"
-//add more temperature title here if needed
+#define TEMP_3 "Temperature 3"
+#define TIME_STAMP_DATE "Time Stamp Date"
+#define QUATERNIONA "Quaternion A"
+#define QUATERNIONB "Quaternion B"
+#define QUATERNIONC "Quaternion C"
+#define QUATERNIOND "Quaternion D"
+#define ETAT_BOARD_ACQUISITION1 "Etat Board Acquisition 1"
+#define ETAT_BOARD_ACQUISITION2 "Etat Board Acquisition 2"
+#define ETAT_BOARD_ACQUISITION3 "Etat Board Acquisition 3"
+#define ETAT_BOARD_ALIM1 "Etat Board Alim 1"
+#define ETAT_BOARD_ALIM2 "Etat Board Alim 2"
+#define ETAT_BOARD_PAYLOAD1 "Etat Board Payload 1"
+#define ALIM_VOLTAGE "Alim Voltage"
+#define ALIM_COURANT "Alim Courant"
+#define PAYLOAD_ANG_SPEEDX "Payload Angle Speed X"
+#define PAYLOAD_ANG_SPEEDY "Payload Angle Speed Y"
+#define PAYLOAD_ANG_SPEEDZ "Payload Angle Speed Z"
+#define CAMERA "Camera"
+#define DEPLOIEMENT "Deploiement"
+
+#endif
 #define STRESS_1 "Stress 1"
+//add more temperature title here if needed
 //add more stress title here if needed
 
 class AcquisitionLogger
 {
     public:
-        AcquisitionLogger(int csPin, String systemType);
-        bool initCard();
-        String generateFilename();
-        void writeHeader();
-        void writeRocketData(RocketPacket packet);
+    AcquisitionLogger(int csPin, String systemType);
+    bool initCard();
+    String generateFilename();
+    void writeHeader();
+    void writeRocketData(RocketPacket packet);
 
     private:
-        int mCsPin;
-        String mSystemType;
-        File mLoggingFile;
+    int mCsPin;
+    String mSystemType;
+    File mLoggingFile;
 };
 #endif
